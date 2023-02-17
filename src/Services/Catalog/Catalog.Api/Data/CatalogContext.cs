8d820ba2324f4d3154a4148f47cb8660d0190792
nadia mohammadi <nadiamuhammadi080@gmail.com>
2023-02-17 16:12:31 +0330

    deploing data layer and business layer

---
+++ src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Api.Entities;
+using Catalog.API.Data;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        //its a kind of dependency Injection
+        //injection the IConfiguration inside constructor
+        //it open a connection with database
+        public CatalogContext(IConfiguration configuration)
+        {
+            //it open a connection to database
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
