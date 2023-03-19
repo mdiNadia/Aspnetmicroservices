8ebeb86370bdf084a9091663846986ec1cc4f243
nadia mohammadi <nadiamuhammadi080@gmail.com>
2023-03-20 00:39:57 +0330

    Basket.APIStartup.cs MassTransit-RabbitMQ Configuration

--- src/Services/Basket/Basket.Api/Startup.cs
+++ src/Services/Basket/Basket.Api/Startup.cs
@@ -1,21 +1,20 @@
 using Basket.Api.GrpcServices;
 using Basket.Api.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.Api
 {
+    //Install-Package MassTransit
+    //Install-Package MassTransit.RabbitMQ
+    //Install-Package MassTransit.AspNetCore
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -39,6 +38,16 @@ namespace Basket.Api
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
                 (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
             services.AddScoped<DiscountGrpcService>();
+
+            // MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config => {
+                config.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+            services.AddMassTransitHostedService();
+
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
